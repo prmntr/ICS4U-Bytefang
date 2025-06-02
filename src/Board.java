f732080d150df768d510f449dc4ca6227209ac1a
Sherlok Chan <sherlokcnchan@gmail.com>
2025-06-02 17:39:20 -0400

    sherlok

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 /**
  * @author Sherlok Chan
  * 2025-05-29
@@ -17,9 +18,40 @@ public class Board {
         this.selectedCharacter = selectedCharacter;
     }
 
+    
     private void initializeBoard() {
-        // what is initalize 
+        Character Chantal = new Character(false, true, false, false, false, false, false, true, false, false, true, false, true, true)
+        CharacterList.add(Chantal);
+        Character Eric = new Character(true, false, true, true, true, false, false, false, false, false, false, false, false, false)
+        CharacterList.add(Eric);
+        Character Alex = new Character(true, false, false, false, false, true, false, false, false, false, false, true, false, true)
+        CharacterList.add(Alex);
+        Character Bob = new Character(true, false, true, true, false, true, false, false, false, false, true, false, false, false)
+        CharacterList.add(Bob);
+        Character Paul = new Character(true, false, true, false, true, false, false, false, true, false, false, false, false, false)
+        CharacterList.add(Paul);
+        Character Frank = new Character(true, false, true, false, false, false, true, false, false, true, true, false, false, true)
+        CharacterList.add(Frank);
+        Character Zoe = new Character(true, false, true, false, false, false, true, false, false, true, true, false, false, true)
+        CharacterList.add(Zoe);
+        
     }
+    /**
+    boolean isMale,
+	boolean wearingGlasses,
+	boolean hasMoustache,
+	boolean wearingBunnyEars,
+	boolean isBald,
+	boolean hasBlondHair,
+	boolean hasBlackHair,
+	boolean hasBrownHair,
+	boolean wearingBandana,
+	boolean wearingHairBand,
+	boolean wearingEarrings,
+	boolean wearingTie,
+	boolean wearingNecklace,
+	boolean mouthOpen) 
+     */
 
     /**
      * This methods iterate over the characteList and remove characters
